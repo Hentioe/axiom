280c9018655866d2c6e7b7a08cfd798ad6768b23
Hentioe <meow.i5.br@gmail.com>
2025-02-18 02:32:29 +0800

    Supports merging reasoning content

--- lib/axiom/helper.ex
+++ lib/axiom/helper.ex
@@ -3,13 +3,19 @@ defmodule Axiom.Helper do
 
   def merge_chunks_content(chunks) when is_list(chunks) do
     Enum.reduce(chunks, "", fn %{"choices" => choices}, content ->
-      content <> merge_choices_content(choices)
+      content <> merge_choices_delta_field(choices, "content")
     end)
   end
 
-  defp merge_choices_content(choices) when is_list(choices) do
+  def merge_chunks_reasoning_content(chunks) when is_list(chunks) do
+    Enum.reduce(chunks, "", fn %{"choices" => choices}, content ->
+      content <> merge_choices_delta_field(choices, "reasoning_content")
+    end)
+  end
+
+  defp merge_choices_delta_field(choices, field) when is_list(choices) do
     Enum.reduce(choices, "", fn %{"delta" => delta}, content ->
-      content <> (delta["content"] || "")
+      content <> (delta[field] || "")
     end)
   end
 end
